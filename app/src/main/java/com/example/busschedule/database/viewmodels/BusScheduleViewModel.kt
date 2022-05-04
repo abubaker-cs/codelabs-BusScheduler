fe9e95581dc2e03db9dcca8c02a0b54163ea8da7
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-05-04 16:57:35 +0500

    Implementing Factory method in ViewModel

--- app/src/main/java/com/example/busschedule/database/viewmodels/BusScheduleViewModel.kt
+++ app/src/main/java/com/example/busschedule/database/viewmodels/BusScheduleViewModel.kt
@@ -5,23 +5,45 @@ import androidx.lifecycle.ViewModelProvider
 import com.example.busschedule.database.schedule.Schedule
 import com.example.busschedule.database.schedule.ScheduleDao
 
+//  It should take a single parameter of type ScheduleDao
 class BusScheduleViewModel(private val scheduleDao: ScheduleDao) : ViewModel() {
 
+    // getAll()
     fun fullSchedule(): List<Schedule> = scheduleDao.getAll()
 
+    // getByStopName()
     fun scheduleForStopName(name: String): List<Schedule> = scheduleDao.getByStopName(name)
 
 }
 
+/**
+ * Why to initialize through Factory
+ * =================================
+ * Instead of directly initializing the ViewModel class, we are going to rely on ModelFactory
+ * to handle Memory Management tasks, otherwise our Fragment will have to perform these additional
+ * tasks manually.
+ *
+ * This will ensure that viewModel will be lifecycle aware without your fragment having to handle this directly
+ *
+ * Factory: This will instantiate view model objects for you.
+ *
+ */
 class BusScheduleViewModelFactory(
     private val scheduleDao: ScheduleDao
 ) : ViewModelProvider.Factory {
 
+    // Returns a BusScheduleViewModelFactory with some error checking
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
+
+        //
         if (modelClass.isAssignableFrom(BusScheduleViewModel::class.java)) {
+
             @Suppress("UNCHECKED_CAST")
             return BusScheduleViewModel(scheduleDao) as T
+
         }
+
+        // Error Handling
         throw IllegalArgumentException("Unknown ViewModel class")
     }
 }
